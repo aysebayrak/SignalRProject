0a2f657a8253c7a8b4f979b76d139e0a9d6e1d7e
AYŞE BAYRAK <ayse.bayrak812@gmail.com>
2023-11-20 23:25:37 +0300

    UI

--- SignalRWebUI/Dtos/ProductDtos/ResultProductDto.cs
+++ SignalRWebUI/Dtos/ProductDtos/ResultProductDto.cs
@@ -2,7 +2,7 @@
 {
     public class ResultProductDto
     {
-        public int ProductID { get; set; }
+        public int ProductId { get; set; }
         public string ProductName { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
