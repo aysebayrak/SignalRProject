e4d492341c5bd36c520eb013461defea65869253
AYŞE BAYRAK <ayse.bayrak812@gmail.com>
2024-01-11 16:06:44 +0300

    menutablerepository

--- SignalRApi/Controllers/MenuTablesController.cs
+++ SignalRApi/Controllers/MenuTablesController.cs
@@ -1,4 +1,6 @@
 ﻿using BusinessLayer.Abstract;
+using DtoLayer.MenuTableDto;
+using EntityLayer.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -21,5 +23,58 @@ namespace SignalRApi.Controllers
 		{
 		   return Ok(	_menuTableService.TMenuTableCount());
 		}
-	}
+
+
+        [HttpGet]
+        public IActionResult MenuTableList()
+        {
+            var values = _menuTableService.TGetListAll();
+            return Ok(values);
+        }
+
+
+        [HttpPost]
+        public IActionResult CreateMenuTable(CreateMenuTableDto createMenuTableDto)
+        {
+            MenuTable menuTable = new MenuTable()
+            {
+               MenuTableName = createMenuTableDto.MenuTableName,
+               MenuTableStatus = false
+
+            };
+            _menuTableService.TAdd(menuTable);
+            return Ok("masa  başarılı bir şekilde eklendi");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteMenuTable(int id)
+        {
+            var value = _menuTableService.TGetById(id);
+            _menuTableService.TDelete(value);
+            return Ok("masa Silindi");
+
+        }
+
+        [HttpPut]
+        public IActionResult UpdateMenuTable(UpdateMenuTableDto updateMenuTableDto)
+        {
+            MenuTable menuTable = new MenuTable()
+            {
+                MenuTableId = updateMenuTableDto.MenuTableId,
+                MenuTableName = updateMenuTableDto.MenuTableName,
+                MenuTableStatus = updateMenuTableDto.MenuTableStatus
+            };
+            _menuTableService.TUpdate(menuTable);
+            return Ok("Masa Güncellendi");
+
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult GetMenuTable(int id)
+        {
+            var value = _menuTableService.TGetById(id);
+            return Ok(value);
+        }
+    }
 }
