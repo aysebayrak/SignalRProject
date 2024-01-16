43eb247ef23c4ad3eaa6db29b517b9bd714614b4
AYŞE BAYRAK <ayse.bayrak812@gmail.com>
2024-01-16 11:27:58 +0300

    mail

--- SignalRWebUI/Controllers/QRCodeController.cs
+++ SignalRWebUI/Controllers/QRCodeController.cs
@@ -8,25 +8,23 @@ namespace SignalRWebUI.Controllers
     public class QRCodeController : Controller
     {
         [HttpGet]
-        public IActionResult Index( )
+        public IActionResult Index()
         {
-
             return View();
         }
 
         [HttpPost]
-       public IActionResult Index(string value)
-          {
+        public IActionResult Index(string value)
+        {
             using (MemoryStream memoryStream = new MemoryStream())
             {
-                QRCodeGenerator qRCodeGenerator = new QRCodeGenerator();
-                QRCodeGenerator.QRCode squareCode = qRCodeGenerator.CreateQrCode(value, QRCodeGenerator.ECCLevel.Q);
-                using (Bitmap image = squareCode.GetGraphic(10))
-                {
-                    image.Save(memoryStream, ImageFormat.Png);
-                    ViewBag.QRCodeImage = "data:image/png;base64,"+Convert.ToBase64String(memoryStream.ToArray());
-                }
-
+                QRCodeGenerator createQRCode = new QRCodeGenerator();
+             //   QRCodeGenerator.QRCode squareCode = createQRCode.CreateQrCode(value, QRCodeGenerator.ECCLevel.Q);
+                //using (Bitmap image = squareCode.GetGraphic(10))
+                //{
+                //    image.Save(memoryStream, ImageFormat.Png);
+                //    ViewBag.QrCodeImage = "data:image/png;base64," + Convert.ToBase64String(memoryStream.ToArray());
+                //}
             }
             return View();
         }
